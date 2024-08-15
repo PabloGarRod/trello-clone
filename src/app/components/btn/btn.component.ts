35387e6b854ca63feaa7bae45eca81d57f4f78f4
Pablo <pablogarcirodriguez@gmail.com>
2024-08-15 11:53:58 +0200

    Colores y propiedades dinámicas en TailwindCSS

--- src/app/components/btn/btn.component.ts
+++ src/app/components/btn/btn.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-btn',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './btn.component.html',
 })
 export class BtnComponent {
   @Input() typeBtn: 'button' | 'reset' | 'submit' = 'button';
+  @Input() colorBtn: 'success' | 'primary' | 'danger' | 'sky' = 'primary';
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  get colors() {
+    return {
+      'bg-success-700': this.colorBtn === 'success',
+      'hover:bg-success-800': this.colorBtn === 'success',
+      'focus:ring-success-300': this.colorBtn === 'success',
+      'bg-primary-700': this.colorBtn === 'primary',
+      'hover:bg-primary-800': this.colorBtn === 'primary',
+      'focus:ring-primary-300': this.colorBtn === 'primary',
+      'bg-danger-700': this.colorBtn === 'danger',
+      'hover:bg-danger-800': this.colorBtn === 'danger',
+      'focus:ring-danger-300': this.colorBtn === 'danger',
+      'bg-sky-700': this.colorBtn === 'sky',
+      'hover:bg-sky-800': this.colorBtn === 'sky',
+      'focus:ring-sky-300': this.colorBtn === 'sky',
+    };
+  }
+
+  get saludo() {
+    return 'hola';
+  }
 }
