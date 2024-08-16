0493fd8385acd19e87871fde77f0fa5a46eb7b5b
Pablo <pablogarcirodriguez@gmail.com>
2024-08-16 12:20:17 +0200

    Iconos en Angular con FontAwesome

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import {
+  faBox,
+  faWaveSquare,
+  faClock,
+} from '@fortawesome/free-solid-svg-icons';
+import { faTrello } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-boards',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FontAwesomeModule],
   templateUrl: './boards.component.html',
 })
-export class BoardsComponent {}
+export class BoardsComponent implements OnInit {
+  faBox = faBox;
+  faWaveSquare = faWaveSquare;
+  faClock = faClock;
+  faTrello = faTrello;
+
+  ngOnInit(): void {}
+}
