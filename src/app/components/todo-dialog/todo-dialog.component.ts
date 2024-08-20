3603793d5d484f22bc16a0be9be2f70cfb66d238
Pablo <pablogarcirodriguez@gmail.com>
2024-08-20 21:02:03 +0200

    Modales recibiendo información

--- src/app/components/todo-dialog/todo-dialog.component.ts
+++ src/app/components/todo-dialog/todo-dialog.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatDialogModule } from '@angular/material/dialog';
-import { DialogRef } from '@angular/cdk/dialog';
+import { DialogRef, DIALOG_DATA } from '@angular/cdk/dialog';
 import {
   faClose,
   faCheckToSlot,
@@ -11,16 +12,24 @@ import {
   faClock,
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
 import { BtnComponent } from '../btn/btn.component';
+import { ToDo } from '../../models/todo.models';
+
+interface InputData {
+  todo: ToDo;
+}
+
+interface OutPutData {
+  rta: boolean;
+}
 
 @Component({
   selector: 'app-todo-dialog',
   standalone: true,
-  imports: [MatDialogModule, BtnComponent, FontAwesomeModule],
+  imports: [MatDialogModule, BtnComponent, FontAwesomeModule, CommonModule],
   templateUrl: './todo-dialog.component.html',
 })
-export class TodoDialogComponent {
+export class TodoDialogComponent implements OnInit {
   faClose = faClose;
   faCheckToSlot = faCheckToSlot;
   faBars = faBars;
@@ -29,9 +38,23 @@ export class TodoDialogComponent {
   faCheckSquare = faCheckSquare;
   faClock = faClock;
 
-  constructor(private dialogRef: DialogRef) {}
+  todo: ToDo;
+
+  constructor(
+    private dialogRef: DialogRef<OutPutData>,
+    @Inject(DIALOG_DATA) data: InputData
+  ) {
+    this.todo = data.todo;
+  }
+
+  ngOnInit(): void {}
 
   close() {
     this.dialogRef.close();
+  }
+  closeWithRta(rta: boolean) {
+    this.dialogRef.close({
+      rta,
+    });
   }
 }
--- src/app/pages/board/board.component.ts
+++ src/app/pages/board/board.component.ts
@@ -94,11 +94,17 @@ export class BoardComponent {
     this.columns.push({ title: 'New Column', todos: [] });
   }
 
-  openDialog() {
-    this.dialog.open(TodoDialogComponent, {
+  openDialog(todo: ToDo) {
+    const dialogRef = this.dialog.open(TodoDialogComponent, {
       minWidth: '300px',
       maxWidth: '50%',
       autoFocus: false,
+      data: {
+        todo: todo,
+      },
+    });
+    dialogRef.closed.subscribe((output) => {
+      console.log('output', output);
     });
   }
 }
