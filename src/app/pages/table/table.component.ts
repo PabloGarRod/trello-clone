ea62d8ddb648f42a1f53ed7aa98be09e1cc0027d
Pablo <pablogarcirodriguez@gmail.com>
2024-08-21 10:59:03 +0200

    Estilos en tablas

--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { CdkTableModule } from '@angular/cdk/table';
 import { NavbarComponent } from '@components/navbar/navbar.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
@@ -7,13 +8,15 @@ import { Product } from '@models/product.model';
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [CdkTableModule, NavbarComponent, HttpClientModule],
+  imports: [CdkTableModule, NavbarComponent, HttpClientModule, CommonModule],
   templateUrl: './table.component.html',
 })
 export class TableComponent implements OnInit {
   products: Product[] = [];
 
   columns: string[] = ['id', 'name', 'price', 'img'];
+
+  total = 0;
 
   constructor(private http: HttpClient) {}
 
@@ -22,6 +25,11 @@ export class TableComponent implements OnInit {
       .get<Product[]>('https://api.escuelajs.co/api/v1/products')
       .subscribe((data) => {
         this.products = data;
+        this.total = this.products
+          .map((item) => item.price)
+          .reduce((price, total) => {
+            return price + total;
+          }, 0);
       });
   }
 }
