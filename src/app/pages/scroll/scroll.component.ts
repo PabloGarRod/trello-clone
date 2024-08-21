8fadb2f523bb3a1940a2b06115c2785cae7df8d2
Pablo <pablogarcirodriguez@gmail.com>
2024-08-21 09:27:01 +0200

    Listas con Virtual Scrolling

--- src/app/pages/scroll/scroll.component.ts
+++ src/app/pages/scroll/scroll.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '@components/navbar/navbar.component';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { Product } from '@models/todo.models';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-scroll',
   standalone: true,
-  imports: [NavbarComponent, HttpClientModule, CommonModule],
+  imports: [NavbarComponent, HttpClientModule, CommonModule, ScrollingModule],
   templateUrl: './scroll.component.html',
 })
 export class ScrollComponent implements OnInit {
